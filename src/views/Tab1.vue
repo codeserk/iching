43f7c216e061e8220aa3226e96d3dfe367ac7069
Jose Camara <jose.camara@coosto.com>
2020-12-15 12:09:54 +0100

    Initial version - basic ionic app

--- src/views/Tab1.vue
+++ src/views/Tab1.vue
@@ -5,24 +5,38 @@
         <ion-title>Tab 1</ion-title>
       </ion-toolbar>
     </ion-header>
-    <ion-content :fullscreen="true">
+    <ion-content class="content" :fullscreen="true">
       <ion-header collapse="condense">
         <ion-toolbar>
           <ion-title size="large">Tab 1</ion-title>
         </ion-toolbar>
       </ion-header>
-    
+
       <ExploreContainer name="Tab 1 page" />
     </ion-content>
   </ion-page>
 </template>
 
 <script lang="ts">
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
-import ExploreContainer from '@/components/ExploreContainer.vue';
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue'
+import ExploreContainer from '@/components/ExploreContainer.vue'
 
-export default  {
+export default {
   name: 'Tab1',
-  components: { ExploreContainer, IonHeader, IonToolbar, IonTitle, IonContent, IonPage }
+  components: {
+    ExploreContainer,
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonPage,
+  },
 }
 </script>
+
+<style scoped>
+.content {
+  background: red;
+  color: blue !important;
+}
+</style>
